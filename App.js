6c91087e683ad81b72877670d1de794ba5ecb44b
death-inspires-me <nana.esseney@gmail.com>
2024-05-30 13:16:45 +0000

    Finished first part (hello section). Starting second part (search section)

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.page}>
-      <View style={styles.helloSection}>
-      <Text style={styles.helloText}>Hello, Devs</Text>
-      <Text style={styles.taskText}>14 Tasks Today</Text>
-    </View>
+      <View style={styles.part1}>
+        <View style={styles.helloSection}>
+          <Text style={styles.helloText}>Hello, Devs</Text>
+          <Text style={styles.taskText}>14 Tasks Today</Text>
+        </View>
+        <Image style={styles.whiteCircle} source={require('./assets/whiteCircle.png')} />
+        <Image style={styles.personIcon} source={require('./assets/personIcon.png')} />
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -19,18 +24,38 @@ const styles = StyleSheet.create({
     backgroundColor: '#E8D1BA',
   },
 
+  part1: {
+    flexDirection: 'row',
+  },
+
   helloSection: {
-    marginTop: 80,
+    marginTop: 70,
     marginLeft: 20,
 
 },
 
   helloText: {
-    fontSize: 24,
+    fontSize: 27,
     fontWeight: 'bold',
   },
 
   taskText: {
-    fontSize: 18,
+    fontSize: 10,
+  },
+
+  whiteCircle: {
+    width: 50,
+    height: 50,
+    marginTop: 70,
+    marginLeft: 285,
+    position: 'absolute',
+  },
+
+  personIcon: {
+    width: 40,
+    height: 40,
+    marginTop: 72,
+    marginLeft: 290,
+    position: 'absolute',
   },
 });
